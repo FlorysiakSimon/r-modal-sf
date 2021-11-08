a0a8d072f7d2ae19b9b0602216d0586eecb97733
simon <simon.florysiak@gmail.com>
2021-11-08 16:42:57 +0100

    v 1.0.4

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-r-modal-sf
+# r-modal-sf
+
+[npm link](https://www.npmjs.com/package/r-modal-sf)
+
+## Installation
+
+```sh
+npm i r-modal-sf
+```
+
+```sh
+import { Modal } from 'r-modal-sf';
+```
+
+## Examples
+
+Here is a simple example of react-modal-sf being used in an app with some custom styles
+
+```jsx
+import React, { useState } from "react";
+
+export const App = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setModalOpen(!modalOpen);
+  };
+  return (
+    <>
+      <div>
+        <button onClick={toggleModal}>Open modal</button>
+      </div>
+
+      <Modal
+        content="test content"
+        modalOpen={modalOpen}
+        modalClose={toggleModal}
+      />
+    </>
+  );
+};
+```
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,34 +1,74 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types'; 
 
-export const Modal = ({content}) => {
+export const Modal = ({content,style,modalOpen,modalClose}) => {
 
-    const [modal, setModal] = useState(false)
-
-    const toggleModal = () =>{
-        setModal(!modal);
+    const defaultStyles = {
+        modal:{
+            width: '100vw',
+            height: '100vh',
+            top: '0',
+            left: '0',
+            right: '0',
+            bottom: '0',
+            position: 'fixed',
+        },
+        overlay:{
+            width: '100vw',
+            height: '100vh',
+            top: '0',
+            left: '0',
+            right: '0',
+            bottom: '0',
+            position: 'fixed',
+            background: 'rgba(49, 49, 49, 0.8)',
+        },
+        content:{
+            position: 'absolute',
+            top: '40%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            lineHeight: '1.4',
+            background: '#f1f1f1',
+            padding: '14px 28px',
+            borderRadius: '3px',
+            maxWidth: '600px',
+            minWidth: '300px',
+        },
+        close:{
+            position: 'absolute',
+            top: '10px',
+            right: '10px',
+            padding: '5px 17px',
+            size:'14px',
+            border:'none',
+            background: '#f1f1f1',
+        }
     }
+
+    if(!modalOpen){return null;}
 
     return (
         <>
-            <div>
-                <button onClick={toggleModal}>Open modal</button>
-            </div>
-            {modal && (
-            <div className="modal">
-                <div onClick={toggleModal} className="modalOverlay"></div>
-                <div className="modalContent"><p>{content}</p>
-                    <button onClick={toggleModal} className="modalClose">
-                        <span className="material-icons">close</span>
+            <div className="modal" style={style && style.modal ? style.modal : defaultStyles.modal}>
+                <div onClick={modalClose}
+                style={style && style.overlay ? style.overlay : defaultStyles.overlay}
+                className="modalOverlay">   
+                </div>
+                <div className="modalContent" style={style && style.content ? style.content : defaultStyles.content}>
+                    <p>{content}</p>
+                    <button onClick={modalClose} style={style && style.close ? style.close : defaultStyles.close} className="modalClose">
+                        X
                     </button>
                 </div>
             </div>
-            )}
         </>
     )
 }
 
 Modal.propTypes ={
-    content: PropTypes.string
-
+    content: PropTypes.string.isRequired,
+    style:PropTypes.object,
+    modalOpen: PropTypes.bool.isRequired,
+    modalClose: PropTypes.func.isRequired,
 }
